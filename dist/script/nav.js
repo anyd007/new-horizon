d2afca8bb91f4620bc058216e78bbb61c1d9c6ef
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-10-24 14:54:06 +0200

    initail commit, create resposive menu

---
+++ dist/script/nav.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector(".menu-btn");
+const burger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-main");
+const navItems = document.querySelectorAll(".nav-main__item");
+
+let showMenu = false;
+
+const toggleMenu = () =>{
+    if(!showMenu){
+        burger.classList.add("open")
+        nav.classList.add("open");
+        navMenu.classList.add("open")
+        navItems.forEach(item=>{
+            item.classList.add("open")
+        })
+        showMenu = true;
+    }
+    else{
+        burger.classList.remove("open");
+        nav.classList.remove("open")
+        navMenu.classList.remove("open")
+        navItems.forEach(item=>{
+            item.classList.remove("open")
+        })
+        showMenu = false;
+    }
+}
+
+menuBtn.addEventListener("click", toggleMenu)
+navMenu.addEventListener("click", toggleMenu)
+
