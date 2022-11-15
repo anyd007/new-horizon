f98074eefa2ca37acd49996daa684480eb477dd2
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-11-15 13:50:00 +0100

    added loader for gallery

--- dist/script/see-more-gallery.js
+++ dist/script/see-more-gallery.js
@@ -9,3 +9,11 @@ document.querySelectorAll('img')
         AOS.refresh()
     )
 );
+
+window.addEventListener("load", ()=>{
+    const loader = document.querySelector(".loader")
+    loader.classList.add("hidden")
+    loader.addEventListener("transitionend", ()=>{
+        document.body.removeChild("loader")
+    })
+})
