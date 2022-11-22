de74b932f5109f6c7616c716fee77d83bc454dc0
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-11-22 11:57:45 +0100

    little style changes

--- dist/script/about.js
+++ dist/script/about.js
@@ -1,8 +1,23 @@
-const members = document.querySelectorAll(".about__members--img")
+const members = document.querySelectorAll(".about__members")
+const images = document.querySelectorAll(".about__members--img")
 
-
-window.addEventListener("load", ()=>{
-    members.forEach(member=>{
-        member.classList.add("open")
+const options ={
+    threshold: 1,
+    rootMargin: "0px 0px -50px 0px",
+   }
+const apperOnScroll = new IntersectionObserver((entries, apperOnScroll)=>{
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return
+      }
+      if(entry.isIntersecting){
+      images.forEach(img=>{
+        img.classList.add("open")
+      })
+    }
+      apperOnScroll.unobserve(entry.target);
     })
-})
+}, options);
+members.forEach((member) => {
+apperOnScroll.observe(member);
+});
