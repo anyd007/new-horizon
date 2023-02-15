82836dcf88966d37b77a57465c06ae691b524fbb
Andrzej Gębala <agebala@neonet.pl>
2023-02-15 11:04:43 +0100

    added logo background animation

--- assets/script/background-animation.js
+++ assets/script/background-animation.js
@@ -11,15 +11,18 @@ const createSquare =() =>{
     const square = document.createElement("span")
     square.classList.add("span-square")
 
-    let size = Math.random() *20;
+    let size = Math.random() *40;
 
     square.style.width = 20 + size + "px";
     square.style.height = 20 + size + "px";
     square.style.top = Math.random() * innerHeight + "px";
     square.style.left = Math.random() * innerWidth + "px";
 
-    const bg = colors[Math.floor(Math.random() * colors.length)]
-    square.style.background = bg
+    // const bg = colors[Math.floor(Math.random() * colors.length)]
+    // square.style.background = bg
+    square.style.background = "url('/images/logo.jpg')"
+    square.style.backgroundRepeat = "no-repeat"
+    square.style.backgroundSize = 'cover'
 
     backgroundAnimation.appendChild(square)
 
