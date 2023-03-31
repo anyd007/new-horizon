f3c685c6b8f6759d00aa75344aff8e2f70050e07
Andrzej Gębala <agebala@neonet.pl>
2023-03-31 14:14:43 +0200

    addind close section countdown

--- assets/script/events.js
+++ assets/script/events.js
@@ -1,8 +1,11 @@
 let couterDisplay = document.querySelector("#counter");
 const br = document.createElement('br');
+const eventSection = document.querySelector('#events')
+const eventMenuBtn = document.querySelector('.event-item')
 
 // ustawiamy datę docelową
 const countDownDate = new Date("Apr 01, 2023 19:00:00").getTime();
+const closeSectionDate = new Date("Apr 02, 2023 00:00:00").getTime();
 
 // wywołujemy funkcję co 1 sekundę
 const counter = setInterval(function () {
@@ -12,6 +15,7 @@ const counter = setInterval(function () {
 
     // obliczamy różnicę między aktualną datą a datą docelową
     const distance = countDownDate - now;
+    const closeSection =  closeSectionDate - now;
 
       // obliczamy dni, godziny, minuty i sekundy pozostałe do daty docelowej
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -23,6 +27,7 @@ const counter = setInterval(function () {
   let daysDisplay = document.createElement("p")
   
   days === 1 ? daysDisplay.textContent = "dzień" : daysDisplay.textContent = "dni"
+
   let secDisplay = seconds <= 9 ? `0${seconds}` : `${seconds}`
 
   if(hours === 1){
@@ -43,6 +48,11 @@ const counter = setInterval(function () {
   if (distance < 0) {
     clearInterval(counter);
     couterDisplay.textContent = "ZACZYNAMY !!!"
+  }
+
+  if (closeSection < 0) {
+    eventMenuBtn.style.display = "none"
+    eventSection.style.display = "none"
   }
 
 }, 1000)
